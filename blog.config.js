ab3442ffd5041a48fbba352b7f8dedc6f0040db4
ssupawat <madebycantart@gmail.com>
2026-07-31 17:02:48 +0700

    fix: restore /blog subdir URL

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 export default {
   tagline:
     "Nothing to see here yet 😅.",
-  url: "https://ssupawat.github.io",
+  url: "https://ssupawat.github.io/blog",
   repo: "https://github.com/ssupawat/blog",
   social: {
     github: "https://github.com/<your-github>",
