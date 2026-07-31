96406f128c11a97fd84b417b757037458dd02c98
ssupawat <madebycantart@gmail.com>
2026-07-31 17:01:54 +0700

    fix: root URL for Actions deploy

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 export default {
   tagline:
     "Nothing to see here yet 😅.",
-  url: "https://ssupawat.github.io/blog",
+  url: "https://ssupawat.github.io",
   repo: "https://github.com/ssupawat/blog",
   social: {
     github: "https://github.com/<your-github>",
